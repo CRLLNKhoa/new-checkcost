d2166ba27f64b5ff8045435698ddb14c96d76139
Lương Nguyễn Khoa <107914230+CRLLNKhoa@users.noreply.github.com>
2024-11-04 19:17:12 +0700

    download

--- components/layouts/download.tsx
+++ components/layouts/download.tsx
@@ -1,6 +1,20 @@
+"use client"
 import React from "react";
 
 function Download() {
+
+    const downloadFile = () => {
+      // URL tải xuống trực tiếp của Google Drive
+      const downloadUrl = 'https://drive.usercontent.google.com/download?id=1f1EokJ92psfzRxPiIXuqfIjXxFUNbZsi';
+  
+      // Tạo thẻ <a> để tải xuống tệp
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.download = 'your_file_name.ext'; // Đặt tên file mong muốn khi tải xuống
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
   return (
     <div className="flex flex-col w-full p-4 gap-4 mt-12">
       <h1
@@ -14,7 +28,7 @@ function Download() {
         Tiện ích Checkcost hoạt động trực tiếp trên các trang web.Checkcost có
         sẵn cho một số trình duyệt phổ biến. Nhần để cài đặt
       </p>
-      <button className="bg-[#3B5695] max-w-lg mx-auto flex hover:scale-105
+      <button onClick={downloadFile} className="bg-[#3B5695] max-w-lg mx-auto flex hover:scale-105
       items-end text-white px-8 py-3 mt-8 rounded-md gap-4 duration-300">
         <img src="/dl.svg" alt="icon" className="size-6 gap-4" /> Tải về máy
       </button>
